7ef609503014a697c44c5c600fcb0e099a9584b2
cdbhosle07 <cdbhosle07@gmail.com>
2023-04-05 16:41:33 +0530

    CategoryServiceImpl changes in the logger

--- src/main/java/com/shruteekatech/electronic/store/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/shruteekatech/electronic/store/services/impl/CategoryServiceImpl.java
@@ -7,6 +7,7 @@ import com.shruteekatech.electronic.store.exceptions.ResourceNotFoundException;
 import com.shruteekatech.electronic.store.helper.Helper;
 import com.shruteekatech.electronic.store.repositories.CategoryRepository;
 import com.shruteekatech.electronic.store.services.CategoryServiceI;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,9 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
-
+@Slf4j
 @Service
 public class CategoryServiceImpl implements CategoryServiceI {
     @Autowired
@@ -29,10 +28,10 @@ public class CategoryServiceImpl implements CategoryServiceI {
     public CategoryDto create(CategoryDto categoryDto) {
 
         //creating categoryId :randomely
-
+        log.info("Initiating the dao call for the create category");
         Category category = modelMapper.map(categoryDto, Category.class);
         Category saveCategory = categoryRepository.save(category);
-
+        log.info("Completed the dao call for the create category");
         return modelMapper.map(saveCategory, CategoryDto.class);
     }
 
@@ -40,6 +39,7 @@ public class CategoryServiceImpl implements CategoryServiceI {
     public CategoryDto update(CategoryDto categoryDto, int categoryId) {
 
         //get category id
+        log.info("Initiating the dao call for the update category data");
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category not foundException"));
 
         //update category details
@@ -47,31 +47,37 @@ public class CategoryServiceImpl implements CategoryServiceI {
         category.setDescription(categoryDto.getDescription());
         category.setCoverImage(categoryDto.getCoverImage());
         Category updateCategory = categoryRepository.save(category);
+        log.info("Completed the dao call for the update category data");
         return modelMapper.map(updateCategory, CategoryDto.class);
     }
 
     @Override
     public void delete(int categoryId) {
+        log.info("Initiating the dao call for the delete category data");
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category not foundException"));
         categoryRepository.delete(category);
+        log.info("Completed the dao call for the delete category data");
 
     }
 
     @Override
     public PageableResponse<CategoryDto> getAll(int pageNumber, int pageSize, String sortBy, String sortDir) {
+        log.info("Initiating the dao call for the get all category data");
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
 
         Sort sort = (sortBy.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
 
         Page<Category> page = categoryRepository.findAll(pageable);
         PageableResponse<CategoryDto> pageableResponse = Helper.getPageableResponse(page, CategoryDto.class);
-
+        log.info("Completed the dao call for the get all category data");
         return pageableResponse;
     }
 
     @Override
     public CategoryDto get(int categoryId) {
+        log.info("Initiating the dao call for the get category by id data");
         Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFoundException("Category not foundException"));
+        log.info("Completed the dao call for the get category by id data");
         return modelMapper.map(category, CategoryDto.class);
     }
 }
